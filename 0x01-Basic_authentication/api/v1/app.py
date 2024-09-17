1634263b830ba770dc8e02025c3843e518730694
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-17 19:59:06 +0300

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,10 +8,12 @@ from flask import abort
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.errorhandler(401)
 def unauthorized_error(error):
     """Return 401 error"""
     return jsonify({"error": "Unauthorized"}), 401
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
