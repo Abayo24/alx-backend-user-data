35f3eee2712955f331115bef7862df58c561243c
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-19 11:28:20 +0300

    Auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,13 +16,11 @@ class Auth:
         """
         return False
 
-
     def authorization_header(self, request=None) -> None:
         """
         authorize header
         """
         return None
-
 
     def current_user(self, request=None) -> None:
         """
@@ -34,7 +32,7 @@ class Auth:
 if __name__ == '__main__':
     """main"""
     a = Auth()
-    
+
     print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
     print(a.authorization_header())
     print(a.current_user())
