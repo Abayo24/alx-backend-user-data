c390766c46789a7eba9a8c4ff1b20f772af26f4a
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-19 13:53:16 +0300

    Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,27 @@ class BasicAuth(Auth):
     """
     Baic Auth
     """
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+         returns the Base64 part of
+         the Authorization header for a Basic Authentication
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(autorization_header, str):
+            return None
+        if  not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[len('Basic '):]
+    
+    
+if  __name__ == "__main__":
+    a = BasicAuth()
+
+    print(a.extract_base64_authorization_header(None))
+    print(a.extract_base64_authorization_header(89))
+    print(a.extract_base64_authorization_header("Holberton School"))
+    print(a.extract_base64_authorization_header("Basic Holberton"))
+    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9u"))
+    print(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA=="))
+    print(a.extract_base64_authorization_header("Basic1234"))
