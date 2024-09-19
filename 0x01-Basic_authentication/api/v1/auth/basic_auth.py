26569dbad73abd9f987e8b4b211fa1fa6cd241e6
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-19 16:39:21 +0300

    Basic - User credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -36,7 +36,7 @@ class BasicAuth(Auth):
         return authorization_header.split(' ')[1]
 
     def decode_base64_authorization_header(self,
-                                           base64_authorization_header: Optional[str]
+                                           base64_authorization_header: str
                                            ) -> Optional[str]:
         """
         Decodes the Base64 part of the Authorization header.
@@ -58,19 +58,21 @@ class BasicAuth(Auth):
         except (Exception):
             return None
         return decoded_base64
-    
+
     def extract_user_credentials(self,
-                                 decoded_base64_authorization_header: Optional[str]
+                                 decoded_base64_authorization_header: str
                                  ) -> Tuple[Optional[str], Optional[str]]:
         """
         Extracts the user email and password from the Base64 decoded value.
 
         Args:
-            decoded_base64_authorization_header (str): The decoded Base64 string.
+            decoded_base64_authorization_header (str):
+            The decoded Base64 string.
 
         Returns:
             tuple: A tuple containing the user email and password.
-            None, None: If the decoded string is invalid or not in the expected format.
+            None, None: If the decoded string is invalid or not in
+            the expected format.
         """
         if decoded_base64_authorization_header is None:
             return None, None
@@ -80,3 +82,14 @@ class BasicAuth(Auth):
             return None, None
         user, pwd = decoded_base64_authorization_header.split(":")
         return user, pwd
+
+
+if __name__ == "__main__":
+    """main"""
+    a = BasicAuth()
+
+    print(a.extract_user_credentials(None))
+    print(a.extract_user_credentials(89))
+    print(a.extract_user_credentials("Holberton School"))
+    print(a.extract_user_credentials("Holberton:School"))
+    print(a.extract_user_credentials("bob@gmail.com:toto1234"))
