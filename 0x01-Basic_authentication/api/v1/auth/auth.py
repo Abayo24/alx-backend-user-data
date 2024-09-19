8ffd317ff9580ad201b9ebaa23464759702e327c
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-19 12:47:34 +0300

    Request validation!

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -49,4 +49,5 @@ if __name__ == '__main__':
     print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
     print(a.require_auth("/api/v1/status", ["/api/v1/status/"]))
     print(a.require_auth("/api/v1/users", ["/api/v1/status/"]))
-    print(a.require_auth("/api/v1/users", ["/api/v1/status/", "/api/v1/stats"]))
+    print(a.require_auth("/api/v1/users", ["/api/v1/status/",
+                                           "/api/v1/stats"]))
