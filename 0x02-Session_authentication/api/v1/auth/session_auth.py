53b67fb165a816186bd010fd0dce51d1ad145c83
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-23 12:38:47 +0300

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ This module provides methods to decode Base64 credentials
 and extract user information for basic HTTP authentication.
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 from models.user import User
 from base64 import b64decode
 from typing import TypeVar
@@ -15,4 +16,12 @@ class SessionAuth(Auth):
     BasicAuth class that implements methods for handling
     Basic Authentication through Base64-encoded credentials.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if not user_id or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4)
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
