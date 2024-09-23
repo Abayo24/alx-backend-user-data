310f46c835f63d498d614d927687d7ad99c0a43f
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-23 14:20:28 +0300

    Logout

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -40,3 +40,17 @@ def login():
         response.set_cookie(session_name, session_id)
 
         return response
+
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'], strict_slashes=False)
+def logout():
+    """
+    return an empty JSON dictionary
+    with the status code 200
+    """
+    from api.v1.app import auth
+    deleted = auth.destroy_session(request)
+    
+    if not deleted:
+        return abort(404)
+    return jsonify({}), 200
