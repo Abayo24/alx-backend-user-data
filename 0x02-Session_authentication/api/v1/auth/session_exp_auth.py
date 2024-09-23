ccd45b0f377c05ae76b94b3d8058760bf8585bdc
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-23 15:22:19 +0300

    Expiration?

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Module that adds an expiration date to a Session ID
+"""
+from api.v1.auth.session_auth import SessionAuth
+from datetime import datetime, timedelta
+from models.user import User
+from os import getenv
+
+
+class SessionExpAuth(SessionAuth):
+    """
+    Session Expiration class
+    """
+    def __init__(self):
+        """
+        Constructor
+        """
+        sess_duration = getenv('SESSION_DURATION')
+
+        try:
+            session_duration = init(sess_duration)
+        except Exception:
+            session_duration = 0
+
+        self.session_duration = session_duration
+
+    def create_session(self, user_id=None):
+        """creates session with expiration"""
+        session_id = super().create_session(user_id)
+        if not session_id:
+            return None
+
+        session_dict = {
+            "user_id": user_id,
+            "created_at": datetime.now()
+        }
+
+        self.user_id_by_session_id[session_id] = session_dict
+        return session_id
+
+    def user_id_for_session_id(self, session_id=None):
+        """gets user_id from session_id"""
+        if not session_id:
+            return None
+        if session_id not in self.user_id_by_session_id.keys():
+            return None
+
+        session_dict = self.user_id_by_session_id.get(session_id)
+        if not session_dict:
+            return
+
+        if self.session_duration <= 0:
+            return session_dict.get("user_id")
+
+        created_at = session_dict.get('created_at')
+        if not created_at:
+            return
+        expired_time = created_at + timedelta(seconds=self.session_duration)
+
+        if datetime.now() > expired_time:
+            return
+        return session_dict.get("user_id")
