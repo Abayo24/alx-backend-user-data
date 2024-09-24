c438ed0b4f2c2f4bf3466ccdc0b16cda924ace8f
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2024-09-24 20:48:52 +0300

    Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Regex-ing"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     return re.sub(f'({"|".join(fields)})=[^{separator}]*', lambda m: f'{m.group(1)}={redaction}', message)
